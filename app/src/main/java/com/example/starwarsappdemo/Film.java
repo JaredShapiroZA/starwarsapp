d4b07fae676b27e1af329e0178c3e077e0d10392
Jared Shapiro <jar.shapiro@gmail.com>
2019-07-19 20:39:46 +0200

    added Film class variables

--- app/src/main/java/com/example/starwarsappdemo/Film.java
+++ app/src/main/java/com/example/starwarsappdemo/Film.java
@@ -2,6 +2,14 @@ package com.example.starwarsappdemo;
 
 public class Film
 {
+    private String title;
+    private String releaseDate;
+    private String directors;
+    private String producers;
+    // image
+    //private String rating;
+    private String characters;
+    private String crawlText;
 
 
 
