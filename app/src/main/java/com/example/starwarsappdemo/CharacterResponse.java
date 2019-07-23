314a1714e5533f9e8aa26dc68d7a38cdcd4294f0
Jared Shapiro <jar.shapiro@gmail.com>
2019-07-23 13:47:36 +0200

    Added CharacterResponse class in order to store character info

---
+++ app/src/main/java/com/example/starwarsappdemo/CharacterResponse.java
@@ -0,0 +1,255 @@
+package com.example.starwarsappdemo;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.List;
+
+public class CharacterResponse
+{
+
+
+
+
+    private int count;
+    private String next;
+    private Object previous;
+    private List<ResultsBean> results;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public Object getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Object previous) {
+        this.previous = previous;
+    }
+
+    public List<ResultsBean> getResults() {
+        return results;
+    }
+
+    public void setResults(List<ResultsBean> results) {
+        this.results = results;
+    }
+
+    public static class ResultsBean implements Parcelable {
+
+
+        private String name;
+        private String height;
+        private String mass;
+        private String hair_color;
+        private String skin_color;
+        private String eye_color;
+        private String birth_year;
+        private String gender;
+        private String homeworld;
+        private String created;
+        private String edited;
+        private String url;
+        private List<String> films;
+        private List<String> species;
+        private List<String> vehicles;
+        private List<String> starships;
+
+        protected ResultsBean(Parcel in) {
+            name = in.readString();
+            height = in.readString();
+            mass = in.readString();
+            hair_color = in.readString();
+            skin_color = in.readString();
+            eye_color = in.readString();
+            birth_year = in.readString();
+            gender = in.readString();
+            homeworld = in.readString();
+            created = in.readString();
+            edited = in.readString();
+            url = in.readString();
+            films = in.createStringArrayList();
+            species = in.createStringArrayList();
+            vehicles = in.createStringArrayList();
+            starships = in.createStringArrayList();
+        }
+
+        public static final Creator<ResultsBean> CREATOR = new Creator<ResultsBean>() {
+            @Override
+            public ResultsBean createFromParcel(Parcel in) {
+                return new ResultsBean(in);
+            }
+
+            @Override
+            public ResultsBean[] newArray(int size) {
+                return new ResultsBean[size];
+            }
+        };
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getHeight() {
+            return height;
+        }
+
+        public void setHeight(String height) {
+            this.height = height;
+        }
+
+        public String getMass() {
+            return mass;
+        }
+
+        public void setMass(String mass) {
+            this.mass = mass;
+        }
+
+        public String getHair_color() {
+            return hair_color;
+        }
+
+        public void setHair_color(String hair_color) {
+            this.hair_color = hair_color;
+        }
+
+        public String getSkin_color() {
+            return skin_color;
+        }
+
+        public void setSkin_color(String skin_color) {
+            this.skin_color = skin_color;
+        }
+
+        public String getEye_color() {
+            return eye_color;
+        }
+
+        public void setEye_color(String eye_color) {
+            this.eye_color = eye_color;
+        }
+
+        public String getBirth_year() {
+            return birth_year;
+        }
+
+        public void setBirth_year(String birth_year) {
+            this.birth_year = birth_year;
+        }
+
+        public String getGender() {
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        public String getHomeworld() {
+            return homeworld;
+        }
+
+        public void setHomeworld(String homeworld) {
+            this.homeworld = homeworld;
+        }
+
+        public String getCreated() {
+            return created;
+        }
+
+        public void setCreated(String created) {
+            this.created = created;
+        }
+
+        public String getEdited() {
+            return edited;
+        }
+
+        public void setEdited(String edited) {
+            this.edited = edited;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public List<String> getFilms() {
+            return films;
+        }
+
+        public void setFilms(List<String> films) {
+            this.films = films;
+        }
+
+        public List<String> getSpecies() {
+            return species;
+        }
+
+        public void setSpecies(List<String> species) {
+            this.species = species;
+        }
+
+        public List<String> getVehicles() {
+            return vehicles;
+        }
+
+        public void setVehicles(List<String> vehicles) {
+            this.vehicles = vehicles;
+        }
+
+        public List<String> getStarships() {
+            return starships;
+        }
+
+        public void setStarships(List<String> starships) {
+            this.starships = starships;
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel parcel, int i) {
+            parcel.writeString(name);
+            parcel.writeString(height);
+            parcel.writeString(mass);
+            parcel.writeString(hair_color);
+            parcel.writeString(skin_color);
+            parcel.writeString(eye_color);
+            parcel.writeString(birth_year);
+            parcel.writeString(gender);
+            parcel.writeString(homeworld);
+            parcel.writeString(created);
+            parcel.writeString(edited);
+            parcel.writeString(url);
+            parcel.writeStringList(films);
+            parcel.writeStringList(species);
+            parcel.writeStringList(vehicles);
+            parcel.writeStringList(starships);
+        }
+    }
+}
